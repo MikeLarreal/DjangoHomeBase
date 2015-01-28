68391af99e8ee11331bd46d928e220c31c81d0d8
MikeLarreal <miguel.larreal26@gmail.com>
2015-01-27 22:11:00 -0430

    Django chapter 3
    chapter3

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from mysite.views import current_datetime, hours_ahead
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^admin/', include(admin.site.urls)),
+)
